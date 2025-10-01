3f54e719090f806d2013347ccf318d24e0009d0d
tiago <tiago.b.antunes2017@gmail.com>
2025-10-01 09:25:29 +0100

    VA01_05

--- src/ficha2_1.java
+++ src/ficha2_1.java
@@ -1,5 +1,8 @@
 public class ficha2_1 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("+-------+");
+        System.out.println("| Tiago |");
+        System.out.println("+-------+");
     }
+
 }
