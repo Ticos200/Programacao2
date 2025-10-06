1c7f3c039c68576704e1605fca036d657e7f2795
tiago <tiago.b.antunes2017@gmail.com>
2025-10-06 17:07:19 +0100

    Cenas

---
+++ Exercicios/exercicios/Modulo3ex/src/Professor.java
@@ -0,0 +1,27 @@
+public class Professor {
+    private String nome;
+    private int idade;
+    private String tipo;
+
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+}
